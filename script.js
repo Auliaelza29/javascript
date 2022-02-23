be96658bd4072718d20fa5ee547106d90f11e7c0
Aulia Elza Marchella <88173311+Auliaelza29@users.noreply.github.com>
2022-02-23 13:07:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function cick(){
+    var nama = document.getElementsById('nama');
+    var nik = document.getElementsById('nik');
+    var alamat = document.getElementsById('alamat');
+
+
+
+}
+    document.getElementsById('nama1').innerHTML = nama;
+    document.getElementsById('nik1').innerHTML=nik;
+    document.getElementById('alamat1').innerHTML=alamat;    
